36dca745f7bfb5b3172a9adf992edc19e8db09e3
TouchyPunchy <julien.vigneron@gmail.com>
2020-05-09 17:18:00 +0200

    Indent with tabs <3

--- src/Player.svelte
+++ src/Player.svelte
@@ -1,329 +1,324 @@
 <script>
-    import { onMount } from 'svelte';
-    import Visualizer from './Visualizer.svelte';
+	import { onMount } from 'svelte';
+	import Visualizer from './Visualizer.svelte';
 
-    let audio;
-    let audio_ctx;
-    let media_source;
-    let time = 0;
-    let duration;
-    let paused = true;
-    let volume = 0.5;
-    let volume_save = volume;
-    let mute = false;
-    let loop = false;
-    let shuffle = false;
-    let viz = false;
+	let audio;
+	let audio_ctx;
+	let media_source;
+	let time = 0;
+	let duration;
+	let paused = true;
+	let volume = 0.5;
+	let volume_save = volume;
+	let mute = false;
+	let loop = false;
+	let shuffle = false;
+	let viz = false;
 
-    let folder = "sounds";
-    let sounds = [];
+	let folder = "sounds";
+	let sounds = [];
 
-    let current_sound_index = 0;
-    $: current_sound = (sounds != null && sounds[current_sound_index] != null) 
-        ? sounds[current_sound_index] 
-        : "";
-    $: current_sound_src = (sounds != null && sounds[current_sound_index] != null) 
-        ? folder + "/" + sounds[current_sound_index] 
-        : "";
+	let current_sound_index = 0;
+	$: current_sound = (sounds != null && sounds[current_sound_index] != null) 
+		? sounds[current_sound_index] 
+		: "";
+	$: current_sound_src = (sounds != null && sounds[current_sound_index] != null) 
+		? folder + "/" + sounds[current_sound_index] 
+		: "";
 
-    let touch_screenX;
-    let touch_screenY;
-    let touch_clientX;
-    let touch_clientY;
-    let touch_target;
+	let touch_screenX;
+	let touch_screenY;
+	let touch_clientX;
+	let touch_clientY;
+	let touch_target;
 
-    onMount(() => {
-        fetch('playlist.json')
-        .then((resp) => resp.json())
-        .then(function(response){
-            sounds = response.songs;
-            if(sounds[current_sound_index] != null){
-                audio = document.getElementById('audio');
-                audio.src = folder + "/" + sounds[current_sound_index];
-                
-            }
-        });
-    });
+	onMount(() => {
+		fetch('playlist.json')
+		.then((resp) => resp.json())
+		.then(function(response){
+			sounds = response.songs;
+			if(sounds[current_sound_index] != null){
+				audio = document.getElementById('audio');
+				audio.src = folder + "/" + sounds[current_sound_index];
+			}
+		});
+	});
 
-    function init_audio_context(){
-        let options = { sampleRate: 44100 }; // defaut sampleRate : 48000 vs 44100 for mp3
-        audio_ctx = new (window.AudioContext || window.webkitAudioContext)(options);
-        // console.log(audio_ctx.sampleRate);
-        media_source = audio_ctx.createMediaElementSource(audio);
-        media_source.connect(audio_ctx.destination);
-    }
+	function init_audio_context(){
+		let options = { sampleRate: 44100 }; // defaut sampleRate : 48000 vs 44100 for mp3
+		audio_ctx = new (window.AudioContext || window.webkitAudioContext)(options);
+		// console.log(audio_ctx.sampleRate);
+		media_source = audio_ctx.createMediaElementSource(audio);
+		media_source.connect(audio_ctx.destination);
+	}
 
-    function toggle_play_pause(){
-        paused = !paused;
-    }
+	function toggle_play_pause(){
+		paused = !paused;
+	}
 
-    function stop(){
-        audio.pause();
-        audio.currentTime = 0;
-    }
+	function stop(){
+		audio.pause();
+		audio.currentTime = 0;
+	}
 
-    function previous_sound(){
-        current_sound_index = (current_sound_index === 0 ) ? sounds.length - 1 : current_sound_index - 1 ;
-        play_sound(current_sound_index);
-        scroll_to_sound();
-    }
+	function previous_sound(){
+		current_sound_index = (current_sound_index === 0 ) ? sounds.length - 1 : current_sound_index - 1 ;
+		play_sound(current_sound_index);
+		scroll_to_sound();
+	}
 
-    function next_sound(){
-        if(loop === false) current_sound_index = (current_sound_index + 1) % sounds.length;
-        if(shuffle === true) current_sound_index = Math.floor(Math.random() * sounds.length);
-        play_sound(current_sound_index);
-        scroll_to_sound();
-    }
+	function next_sound(){
+		if(loop === false) current_sound_index = (current_sound_index + 1) % sounds.length;
+		if(shuffle === true) current_sound_index = Math.floor(Math.random() * sounds.length);
+		play_sound(current_sound_index);
+		scroll_to_sound();
+	}
 
-    function toggle_shuffle(){
-        shuffle = !shuffle;
-    }
+	function toggle_shuffle(){
+		shuffle = !shuffle;
+	}
 
-    function toggle_loop(){
-        loop = !loop;
-    }
+	function toggle_loop(){
+		loop = !loop;
+	}
 
-    function toggle_volume_mute(){
-        if(mute === false){
-            volume_save = volume;
-            volume = 0;
-        }else{
-            volume = volume_save;
-        }
-        mute = !mute;
-    }
+	function toggle_volume_mute(){
+		if(mute === false){
+			volume_save = volume;
+			volume = 0;
+		}else{
+			volume = volume_save;
+		}
+		mute = !mute;
+	}
 
-    function toggle_viz(){
-        if(!audio_ctx) init_audio_context();
-        viz = ! viz;
-    }
+	function toggle_viz(){
+		if(!audio_ctx) init_audio_context();
+		viz = ! viz;
+	}
 
-    function volume_up(){
-        if(volume + 0.1 <= 1) volume += 0.1;
-    }
+	function volume_up(){
+		if(volume + 0.1 <= 1) volume += 0.1;
+	}
 
-    function volume_down(){
-        if(volume - 0.1 >= 0) volume -= 0.1;
-    }
+	function volume_down(){
+		if(volume - 0.1 >= 0) volume -= 0.1;
+	}
 
-    function format(seconds) {
+	function format(seconds) {
 		if (isNaN(seconds)) return '...';
 		const minutes = Math.floor(seconds / 60);
 		seconds = Math.floor(seconds % 60);
 		if (seconds < 10) seconds = '0' + seconds;
 		return `${minutes}:${seconds}`;
-    }
-    
-    function handleMousemove(e) {
+	}
+	
+	function handleMousemove(e) {
 		if (!(e.buttons & 1)) return; // mouse not down
-        if (!duration) return; // video not loaded yet
+		if (!duration) return; // video not loaded yet
 		const { left, right } = this.getBoundingClientRect();
 		time = duration * (e.clientX - left) / (right - left);
-    }
-
-    function handleTouchmove(e) {
-        if(e.changedTouches.length > 0){
-            let theTouch = e.changedTouches[0];
-            touch_screenX = theTouch.screenX;
-            touch_screenY = theTouch.screenY;
-            touch_clientX = theTouch.clientX;
-            touch_clientY = theTouch.clientY;
-            touch_target = theTouch.target;
-        }
-        let mouseEv;
-        switch(e.type)
-        {
-            case "touchstart": mouseEv="mousedown"; break;  
-            case "touchend":   mouseEv="mouseup"; break;
-            case "touchmove":  mouseEv="mousemove"; break;
-            default: return;
-        }
-        let mouseEvent = new MouseEvent(mouseEv,{ 
-            screenX: touch_screenX, 
-            screenY: touch_screenY,
-            clientX: touch_clientX,
-            clientY: touch_clientY,
-            button: 0,
-            buttons: 1
-        });
-        touch_target.dispatchEvent(mouseEvent);
-        e.preventDefault();
 	}
 
-    function play_sound(sound_index){
-        current_sound_index = sound_index;
-        audio.src = folder + "/" + sounds[current_sound_index];
-        audio.play();
-    }
+	function handleTouchmove(e) {
+		if(e.changedTouches.length > 0){
+			let theTouch = e.changedTouches[0];
+			touch_screenX = theTouch.screenX;
+			touch_screenY = theTouch.screenY;
+			touch_clientX = theTouch.clientX;
+			touch_clientY = theTouch.clientY;
+			touch_target = theTouch.target;
+		}
+		let mouseEv;
+		switch(e.type)
+		{
+			case "touchstart": mouseEv="mousedown"; break;  
+			case "touchend":   mouseEv="mouseup"; break;
+			case "touchmove":  mouseEv="mousemove"; break;
+			default: return;
+		}
+		let mouseEvent = new MouseEvent(mouseEv,{ 
+			screenX: touch_screenX, 
+			screenY: touch_screenY,
+			clientX: touch_clientX,
+			clientY: touch_clientY,
+			button: 0,
+			buttons: 1
+		});
+		touch_target.dispatchEvent(mouseEvent);
+		e.preventDefault();
+	}
 
-    function scroll_to_sound(){
-        let elt = document.getElementById("item_"+current_sound_index);
-        elt.scrollIntoView({behavior: "smooth"});
-    }
+	function play_sound(sound_index){
+		current_sound_index = sound_index;
+		audio.src = folder + "/" + sounds[current_sound_index];
+		audio.play();
+	}
+
+	function scroll_to_sound(){
+		let elt = document.getElementById("item_"+current_sound_index);
+		elt.scrollIntoView({behavior: "smooth"});
+	}
 </script>
 
 <div class="container">
-    
-    <div class="playlist bg-dark text-light">
-        <div class="playlist_items_wrapper">
-            <div class="playlist_items bg-light text-dark">
-                {#each sounds as sound, i }
-                    <div class="playlist_item" id="item_{i}" class:selected={ current_sound_index === i} on:click={() => play_sound(i)}>{i+1}. {sound}</div>
-                {/each}
-            </div>
-        </div>
-    </div>
-    <div class="player bg-dark"> 
-        <audio id="audio"
-            on:ended={next_sound}
-            bind:paused 
-            bind:volume 
-            bind:duration
-            bind:currentTime={time}
-            ></audio>
-        {#if viz === true}
-        <Visualizer bind:audio_ctx bind:media_source/>
-        {/if}
-        <div class="info text-light">
-            <span class="time"><strong>{format(time)} / {format(duration)}</strong> | </span>{current_sound}
-        </div>
-        <div class="progress">
-            <progress class="bg-light"
-                value="{(time / duration) || 0}"
-                on:mousemove={handleMousemove}
-                on:touchstart={handleTouchmove}
-                on:touchmove={handleTouchmove} 
-                on:touchend={handleTouchmove}/>
-        </div>
-        <div class="controls">
-            <div class="flexbox">
-                <button title="Previous track" 
-                    on:click={previous_sound}><i class='fas fa-step-backward'></i></button> 
-                <button title="Play / Pause"
-                    on:click={toggle_play_pause}>
-                {#if paused === true}
-                    <span><i class='fas fa-play'></i></span>
-                {:else}
-                    <span><i class='fas fa-pause'></i></span>
-                {/if}
-                </button> 
-                <button title="Stop"
-                    on:click={stop}><i class='fas fa-stop'></i></button> 
-                <button title="Next track"
-                    on:click={next_sound}><i class='fas fa-step-forward'></i></button> 
-            </div>
-            <div class="flexbox">
-                <button title="Toggle Shuffle"
-                    class:bg-primary={shuffle} on:click={toggle_shuffle}><i class='fas fa-random'></i></button> 
-                <button title="Toggle Track Loop"
-                    class:bg-primary={loop} on:click={toggle_loop}><i class='fas fa-redo'></i></button> 
-                <button title="Mute / Unmute" class:bg-primary={mute} on:click={toggle_volume_mute}><i class='fas fa-volume-mute'></i></button> 
-                <button title="Toggle Waveform" 
-                    class:bg-primary={viz} on:click={toggle_viz}><i class='fas fa-signal'></i></button> 
-                <!-- <button on:click={volume_down}><i class='fas fa-volume-down'></i></button> 
-                <button on:click={volume_up}><i class='fas fa-volume-up'></i></button> -->
-            </div>
-        </div>
-    </div>  
+	
+	<div class="playlist bg-dark text-light">
+		<div class="playlist_items_wrapper">
+			<div class="playlist_items bg-light text-dark">
+				{#each sounds as sound, i }
+					<div class="playlist_item" id="item_{i}" class:selected={ current_sound_index === i} on:click={() => play_sound(i)}>{i+1}. {sound}</div>
+				{/each}
+			</div>
+		</div>
+	</div>
+	<div class="player bg-dark"> 
+		<audio id="audio"
+			on:ended={next_sound}
+			bind:paused 
+			bind:volume 
+			bind:duration
+			bind:currentTime={time}
+			></audio>
+		{#if viz === true}
+		<Visualizer bind:audio_ctx bind:media_source/>
+		{/if}
+		<div class="info text-light">
+			<span class="time"><strong>{format(time)} / {format(duration)}</strong> | </span>{current_sound}
+		</div>
+		<div class="progress">
+			<progress class="bg-light"
+				value="{(time / duration) || 0}"
+				on:mousemove={handleMousemove}
+				on:touchstart={handleTouchmove}
+				on:touchmove={handleTouchmove} 
+				on:touchend={handleTouchmove}/>
+		</div>
+		<div class="controls">
+			<div class="flexbox">
+				<button title="Previous track" 
+					on:click={previous_sound}><i class='fas fa-step-backward'></i></button> 
+				<button title="Play / Pause"
+					on:click={toggle_play_pause}>
+				{#if paused === true}
+					<span><i class='fas fa-play'></i></span>
+				{:else}
+					<span><i class='fas fa-pause'></i></span>
+				{/if}
+				</button> 
+				<button title="Stop"
+					on:click={stop}><i class='fas fa-stop'></i></button> 
+				<button title="Next track"
+					on:click={next_sound}><i class='fas fa-step-forward'></i></button> 
+			</div>
+			<div class="flexbox">
+				<button title="Toggle Shuffle"
+					class:bg-primary={shuffle} on:click={toggle_shuffle}><i class='fas fa-random'></i></button> 
+				<button title="Toggle Track Loop"
+					class:bg-primary={loop} on:click={toggle_loop}><i class='fas fa-redo'></i></button> 
+				<button title="Mute / Unmute" class:bg-primary={mute} on:click={toggle_volume_mute}><i class='fas fa-volume-mute'></i></button> 
+				<button title="Toggle Waveform" 
+					class:bg-primary={viz} on:click={toggle_viz}><i class='fas fa-signal'></i></button> 
+				<!-- <button on:click={volume_down}><i class='fas fa-volume-down'></i></button> 
+				<button on:click={volume_up}><i class='fas fa-volume-up'></i></button> -->
+			</div>
+		</div>
+	</div>  
 </div>
 <style>
-    :root{
-        --light-color: white;
-        --dark-color: #2b2b2b;
-        --primary-color: #ff9900;
-    }
-    .text-light{ color: var(--light-color); }
-    .text-dark{ color: var(--dark-color); }
-    .bg-primary, .selected{ background-color: var(--primary-color); }
-    .bg-dark{ background-color: var(--dark-color); }
-    .bg-light{ background-color: var(--light-color); }
+	:root{
+		--light-color: white;
+		--dark-color: #2b2b2b;
+		--primary-color: #ff9900;
+	}
+	.text-light{ color: var(--light-color); }
+	.text-dark{ color: var(--dark-color); }
+	.bg-primary, .selected{ background-color: var(--primary-color); }
+	.bg-dark{ background-color: var(--dark-color); }
+	.bg-light{ background-color: var(--light-color); }
 
-    button{
-        background-color: var(--dark-color);
-        color: var(--light-color);
-    }
+	button{
+		background-color: var(--dark-color);
+		color: var(--light-color);
+	}
 
-    button{
-        height: 3.5em;
-        cursor: pointer;
-        border: var(--dark-color) 1px solid;
-        border-radius: 0;
-    }
-    button:focus{
-        outline:none;
-        border: var(--primary-color) 1px solid;
-    }
-    button::-moz-focus-inner {
-        border: 0;
-        padding: 1em;
-    }
-    .container{
-        display: grid;
-        height: 100vh;
-        padding: 0.5em 1em 1em 1em;
-        box-sizing: border-box;
-        grid-template-rows: [row1-start] 1fr [row1-end] auto [last-line];
-    }
-    /* ---- Player ---- */
+	button{
+		height: 3.5em;
+		cursor: pointer;
+		border: var(--dark-color) 1px solid;
+		border-radius: 0;
+	}
+	button:focus{
+		outline:none;
+		border: var(--primary-color) 1px solid;
+	}
+	button::-moz-focus-inner {
+		border: 0;
+		padding: 1em;
+	}
+	.container{
+		display: grid;
+		height: 100vh;
+		padding: 0.5em 1em 1em 1em;
+		box-sizing: border-box;
+		grid-template-rows: [row1-start] 1fr [row1-end] auto [last-line];
+	}
+	/* ---- Player ---- */
 	.player{
-        padding: 0em 1em 1em 1em ;
-        /* Required for text-overflow to do anything */
-        min-width: 0;
-    }
-    .info{
-        padding-top: 1em;
-        padding-bottom : 2em;
-        text-overflow: ellipsis;
-        /* Required for text-overflow to do anything */
-        white-space: nowrap;
-        overflow: hidden;
-    }
-    .progress{ padding-bottom : 2em; }
-    progress {
-        border: 0;
+		padding: 0em 1em 1em 1em ;
+		/* Required for text-overflow to do anything */
+		min-width: 0;
+	}
+	.info{
+		padding-top: 1em;
+		padding-bottom : 2em;
+		text-overflow: ellipsis;
+		/* Required for text-overflow to do anything */
+		white-space: nowrap;
+		overflow: hidden;
+	}
+	.progress{ padding-bottom : 2em; }
+	progress {
+		border: 0;
 		display: block;
 		width: 100%;
 		height: 1em;
 		-webkit-appearance: none;
-        appearance: none;
-    }
+		appearance: none;
+	}
 	progress::-webkit-progress-bar { background-color: var(--light-color); }
-    progress::-moz-progress-bar { background-color: var(--primary-color); }
+	progress::-moz-progress-bar { background-color: var(--primary-color); }
 	progress::-webkit-progress-value { background-color: var(--primary-color); }
    
-    .flexbox{
-        display:flex;
-    }
-    .flexbox button{
-        flex: 1 1 0px;
-    }
+	.flexbox{ display:flex; }
+	.flexbox button{ flex: 1 1 0px; }
 
-    /* ---- Playlist ---- */
-    .playlist{
-        overflow: hidden;
-        padding: 1em;
-    }
-    .playlist_items_wrapper{
-        height: 100%;
-        max-height: 100%;
-        overflow-y: scroll;
-        scrollbar-width: none;
-        background-color: var(--light-color)
-    }
-    .playlist_items_wrapper::-webkit-scrollbar{ display: none; }
-    .playlist_items{
-        max-height: 100%;
-        min-width: 0;
-    }
-    .playlist_item{
-        cursor: pointer;
-        padding: 0.5em;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        overflow: hidden;
-    }
+	/* ---- Playlist ---- */
+	.playlist{
+		overflow: hidden;
+		padding: 1em;
+	}
+	.playlist_items_wrapper{
+		height: 100%;
+		max-height: 100%;
+		overflow-y: scroll;
+		scrollbar-width: none;
+		background-color: var(--light-color)
+	}
+	.playlist_items_wrapper::-webkit-scrollbar{ display: none; }
+	.playlist_items{
+		max-height: 100%;
+		min-width: 0;
+	}
+	.playlist_item{
+		cursor: pointer;
+		padding: 0.5em;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		overflow: hidden;
+	}
 
-    @media (max-width: 640px) {
+	@media (max-width: 640px) {
 		.container {
 			padding: 0;
 		}
--- src/Visualizer.svelte
+++ src/Visualizer.svelte
@@ -1,108 +1,107 @@
 <script>
-    import { onMount, onDestroy } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 
-    export let audio_ctx;
-    export let media_source;
+	export let audio_ctx;
+	export let media_source;
 
-    // https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API
-    const bg_color = '#2b2b2b';
-    const stroke_color = '#ff9900';
-    let viz_index = 0;
-    let canvas;
-    let ctx;
-    let w;
-    let h;
-    let analyser;
-    let draw_visual; 
+	// https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API
+	const bg_color = '#2b2b2b';
+	const stroke_color = '#ff9900';
+	let viz_index = 0;
+	let canvas;
+	let ctx;
+	let w;
+	let h;
+	let analyser;
+	let draw_visual; 
 
-    onMount(() => {
-        ctx = canvas.getContext('2d');
-        w = ctx.canvas.width;
-        h = ctx.canvas.height;
-        analyser = audio_ctx.createAnalyser();
-        analyser.minDecibels = -70;
-        //analyser.maxDecibels = -10;
-        media_source.connect(analyser);
-        draw_viz();
-    });
-
-    onDestroy(() => {
-        console.log('the component is being destroyed');
-        cancelAnimationFrame(draw_visual);
-        draw_visual = undefined;
-        media_source.disconnect(analyser);
+	onMount(() => {
+		ctx = canvas.getContext('2d');
+		w = ctx.canvas.width;
+		h = ctx.canvas.height;
+		analyser = audio_ctx.createAnalyser();
+		analyser.minDecibels = -70;
+		//analyser.maxDecibels = -10;
+		media_source.connect(analyser);
+		draw_viz();
 	});
 
-    function draw_viz(){
-        if(viz_index === 0){
-            draw_waveform();
-            viz_index += 1;
-        }else{
-            draw_frequencybars();
-            viz_index = 0;
-        }
-    }
+	onDestroy(() => {
+		cancelAnimationFrame(draw_visual);
+		draw_visual = undefined;
+		media_source.disconnect(analyser);
+	});
 
-    function draw_waveform(){
-        analyser.fftSize = 2048;
-        let bufferLength = analyser.frequencyBinCount;
-        let dataArray = new Uint8Array(bufferLength);
-        analyser.getByteTimeDomainData(dataArray);
-        ctx.clearRect(0, 0, w, h);
-        function draw() {
-            draw_visual = requestAnimationFrame(draw);
-            analyser.getByteTimeDomainData(dataArray);
-            ctx.fillStyle = bg_color;
-            ctx.fillRect(0, 0, w, h);
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = stroke_color;
-            ctx.beginPath();
-            let sliceWidth = w * 1.0 / bufferLength;
-            let x = 0;
-            for(let i = 0; i < bufferLength; i++) {
-                let v = dataArray[i] / 128.0 ;
-                let y = v * h / 2;
-                if(i === 0)
-                    ctx.moveTo(x, y);
-                else
-                    ctx.lineTo(x, y);
-                x += sliceWidth;
-            }
-            ctx.lineTo(w, h / 2);
-            ctx.stroke();
-        }
-        draw();
-    }
+	function draw_viz(){
+		if(viz_index === 0){
+			draw_waveform();
+			viz_index += 1;
+		}else{
+			draw_frequencybars();
+			viz_index = 0;
+		}
+	}
 
-    function draw_frequencybars(){
-        analyser.fftSize = 64;
-        let bufferLength = analyser.frequencyBinCount;
-        let dataArray = new Uint8Array(bufferLength);
-        ctx.clearRect(0, 0, w, h);
+	function draw_waveform(){
+		analyser.fftSize = 2048;
+		let bufferLength = analyser.frequencyBinCount;
+		let dataArray = new Uint8Array(bufferLength);
+		analyser.getByteTimeDomainData(dataArray);
+		ctx.clearRect(0, 0, w, h);
+		function draw() {
+			draw_visual = requestAnimationFrame(draw);
+			analyser.getByteTimeDomainData(dataArray);
+			ctx.fillStyle = bg_color;
+			ctx.fillRect(0, 0, w, h);
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = stroke_color;
+			ctx.beginPath();
+			let sliceWidth = w * 1.0 / bufferLength;
+			let x = 0;
+			for(let i = 0; i < bufferLength; i++) {
+				let v = dataArray[i] / 128.0 ;
+				let y = v * h / 2;
+				if(i === 0)
+					ctx.moveTo(x, y);
+				else
+					ctx.lineTo(x, y);
+				x += sliceWidth;
+			}
+			ctx.lineTo(w, h / 2);
+			ctx.stroke();
+		}
+		draw();
+	}
 
-        function draw() {
-            draw_visual = requestAnimationFrame(draw);
-            analyser.getByteFrequencyData(dataArray);
-            ctx.fillStyle = bg_color;
-            ctx.fillRect(0, 0, w, h);
-            let barWidth = Math.floor((w / bufferLength) * 2);
-            let barHeight;
-            let x = 0;
-            for(var i = 0; i < bufferLength; i++) {
-                barHeight = dataArray[i] / 2;
-                ctx.fillStyle = 'rgb(' + (barHeight + 100) + ',100,00)';
-                ctx.fillRect(x,h - barHeight, barWidth, barHeight);
-                x += barWidth + 1;
-            }
-        }
-        draw();
-    }
+	function draw_frequencybars(){
+		analyser.fftSize = 64;
+		let bufferLength = analyser.frequencyBinCount;
+		let dataArray = new Uint8Array(bufferLength);
+		ctx.clearRect(0, 0, w, h);
+
+		function draw() {
+			draw_visual = requestAnimationFrame(draw);
+			analyser.getByteFrequencyData(dataArray);
+			ctx.fillStyle = bg_color;
+			ctx.fillRect(0, 0, w, h);
+			let barWidth = Math.floor((w / bufferLength) * 2);
+			let barHeight;
+			let x = 0;
+			for(var i = 0; i < bufferLength; i++) {
+				barHeight = dataArray[i] / 2;
+				ctx.fillStyle = 'rgb(' + (barHeight + 100) + ',100,00)';
+				ctx.fillRect(x,h - barHeight, barWidth, barHeight);
+				x += barWidth + 1;
+			}
+		}
+		draw();
+	}
 </script>
-    <canvas bind:this={canvas} on:click={draw_viz}></canvas>
+	<canvas bind:this={canvas} on:click={draw_viz}></canvas>
 <style>
-    canvas{
-        margin-bottom: 0.5em;
-        width: 100%;
-        height: 6em;
-    }
+	canvas{
+		margin-bottom: 0.5em;
+		width: 100%;
+		height: 6em;
+	}
 </style>
